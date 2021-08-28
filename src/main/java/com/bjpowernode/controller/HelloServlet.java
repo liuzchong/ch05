903e6be4fe7e71baff4dfdc273ef582447019f8e
Liuzc <liuzchong@163.com>
2021-08-28 22:52:50 +0800

    first commit

---
+++ src/main/java/com/bjpowernode/controller/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.bjpowernode.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author liuzc
+ * @create 2021-08-28 14:48
+ */
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("Hello Maven Web");
+        pw.flush();
+        pw.close();
+    }
+}
